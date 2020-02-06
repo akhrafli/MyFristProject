008c548cc3db957bc1b4c3771b121337b91fb22d
Akhrafli <rafliabd017@gmail.com>
2020-02-07 02:52:20 +0700

    mengubah Mainactivty menambah Button

--- app/src/main/java/com/example/myfristproject/MainActivity.java
+++ app/src/main/java/com/example/myfristproject/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myfristproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int a = 10;
+
+        Button btnPindah = findViewById(R.id.btn);
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent ( MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
     }
 }
